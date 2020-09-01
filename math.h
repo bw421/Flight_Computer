bd593ff336054ef31a4cf1cc97d7a974cf24142b
bw421 <64450746+bw421@users.noreply.github.com>
2020-08-31 21:45:13 -0400

    Add Kronecker Product

--- math.h
+++ math.h
@@ -18,3 +18,19 @@ Quaternion ToQuaternion(double yaw, double pitch, double roll){
 
   return q;
 }
+
+Quaternion K_Product(Quaternion q_m, Quaternion q_r){
+  //This function will perform a Kronecker Product multiplication
+  //on two quaternions: q_m = the measured state
+  //                    q_r = the desired state
+  //And output the error correction required to go from q_m to q_r:
+  //                    q_err = the error quaternion
+  
+  Quaternion q_err;
+  q_err.w = q_m.w*q_r.w - q_m.x*q_r.x - q_m.y*q_r.y - q_m.z*q_r.z;
+  q_err.x = q_m.w*q_r.x - q_m.x*q_r.w - q_m.y*q_r.z - q_m.z*q_r.y;
+  q_err.y = q_m.w*q_r.y - q_m.x*q_r.z - q_m.y*q_r.w - q_m.z*q_r.x;
+  q_err.z = q_m.w*q_r.z - q_m.x*q_r.y - q_m.y*q_r.x - q_m.z*q_r.w;
+
+  return q_err;  
+}
